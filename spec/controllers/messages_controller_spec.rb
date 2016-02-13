89e576d688c0f72b69082a8f0ddd2a3b13d75555
SENDA Keijiro <senda.keijiro@gmail.com>
2016-02-13 12:48:51 +0000

    Add message controller test cases

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
+  before do
+    create(:message)
+  end
 
+  describe '#index' do
+    it 'return index' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe '#create' do
+    it 'saves message' do
+      expect do
+        post :create, message: { message: 'foobar' }
+      end.to change(Message, :count).by(1)
+      expect(response).to render_template(:index)
+    end
+    it 'does not save message without valid content' do
+      expect do
+        post :create, message: { message: ' ' }
+      end.not_to change(Message, :count)
+      expect(response).to render_template(:index)
+    end
+  end
 end

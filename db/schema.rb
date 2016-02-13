18706c43992b40121a3039b0d3f382e6c2b7dc02
SENDA Keijiro <senda.keijiro@gmail.com>
2016-02-13 09:37:12 +0000

    Add Meesages controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208195701) do
+ActiveRecord::Schema.define(version: 20160213090811) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
